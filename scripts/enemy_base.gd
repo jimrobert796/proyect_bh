1a924d0178c93825e1c5c2d822dfc2a22f54a7bb
jimrobert796 <jimmyroberto270@gmail.com>
2026-09-13 20:59:08 -0600

    Ejemplo a pulir movimiento de boss

--- scripts/enemy_base.gd
+++ scripts/enemy_base.gd
@@ -9,16 +9,26 @@ extends Area2D
 ] # Array de patterns a usar
 @export var loop = true  # si al terminar la lista, vuelve a empezar desde el principio
 
+# Movimiento simple izquierda-derecha
+@export var amplitud_horizontal: float = 150.0  # cuánto se mueve a cada lado desde el origen
+@export var velocidad_horizontal: float = 1.0   # qué tan rápido va y viene
 
 var vida
 var patternActual: Pattern
 var indiceActual = 0
+var origen_x: float
+var tiempo_movimiento = 0.0
 
 func _ready():
 	#Logica a construir (fases, etc lo que sea)
 	vida = vidaMax
+	origen_x = global_position.x  # guarda su posición horizontal inicial como centro del vaivén
 	if secuenciaPatterns.size() > 0:
 		asignar_pattern(secuenciaPatterns[0])
+
+func _process(delta):
+	tiempo_movimiento += delta
+	global_position.x = origen_x + sin(tiempo_movimiento * velocidad_horizontal) * amplitud_horizontal
 
 func asignar_pattern(nuevoPatternScene: PackedScene):
 	if patternActual:

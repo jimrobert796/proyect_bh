1d3b830bb460a4ff11b5bced5ad4ebc28a055775
jimrobert796 <jimmyroberto270@gmail.com>
2026-09-13 21:08:28 -0600

    Escena player no depende de main ahora, escena independiente

--- main.gd
+++ main.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 var enemyScene = preload("res://scenes/enemy_base.tscn")
 var enemyMini = preload("res://scenes/enemy_mini.tscn")
+var player = preload("res://scenes/player.tscn")
 
 func _ready():
+	var plyr = player.instantiate()
+	add_child(plyr)
+	plyr.global_position = Vector2(600, 600)
+	
 	var e1 = enemyScene.instantiate()
 	add_child(e1)
 	e1.global_position = Vector2(200, 100)
